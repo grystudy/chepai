a01aab274725dd4d57bf588ab2a91dc7859b5c80
yangy <yangy@meixing.com>
2016-10-10 17:18:32 +0800

    协议修改及导出数据

--- app/models/plate_number.rb
+++ app/models/plate_number.rb
@@ -1,12 +1,12 @@
 require 'date'
 class PlateNumber < ApplicationRecord
-  has_many :uu_che_pais
+  has_many :uu_che_pais, class_name: "UUChePai"
   has_many :che_pais
   has_many :weizhang_queries,-> { includes :weizhang_items }
 
   def need_requery?
     last_query = weizhang_queries.order(time: :desc).first
     return true unless last_query
-    (DateTime.now - last_query.time.to_datetime).abs.to_f > 50
+    (DateTime.now - last_query.time.to_datetime).abs.to_f > 100
   end
 end
